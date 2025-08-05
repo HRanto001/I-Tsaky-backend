da6a034747407f2fdf23513db3c17631cd78598d
h-ranto <hei.ranto.2@gmail.com>
2025-08-05 16:53:01 +0300

    chore: add packages and entity to scan

--- src/main/java/ranto/co/io/PojaApplication.java
+++ src/main/java/ranto/co/io/PojaApplication.java
@@ -2,11 +2,14 @@ package ranto.co.io;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 @PojaGenerated
 @EnableScheduling
+@SpringBootApplication(scanBasePackages = "ranto.co.io")
+@EntityScan(basePackages = "ranto.co.io.entity")
 public class PojaApplication {
 
   public static void main(String[] args) {
