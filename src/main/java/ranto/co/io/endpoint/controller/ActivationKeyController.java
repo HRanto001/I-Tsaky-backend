f277dd841c89f34cc1cd61a16615cf16a69afa9e
ranto-x <ranto.rafalimanana.2@gmail.com>
2025-08-26 19:01:28 +0300

    fix: add delete controller

--- src/main/java/ranto/co/io/endpoint/controller/ActivationKeyController.java
+++ src/main/java/ranto/co/io/endpoint/controller/ActivationKeyController.java
@@ -1,8 +1,11 @@
 package ranto.co.io.endpoint.controller;
 
 import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ranto.co.io.model.ActivationKey;
+import ranto.co.io.repository.ActivationKeyRepository;
 import ranto.co.io.service.ActivationKeyService;
 
 @RestController
@@ -10,9 +13,11 @@ import ranto.co.io.service.ActivationKeyService;
 public class ActivationKeyController {
 
   private final ActivationKeyService activationKeyService;
+  private final ActivationKeyRepository activationKeyRepository;
 
-  public ActivationKeyController(ActivationKeyService activationKeyService) {
+  public ActivationKeyController(ActivationKeyService activationKeyService, ActivationKeyRepository activationKeyRepository) {
     this.activationKeyService = activationKeyService;
+      this.activationKeyRepository = activationKeyRepository;
   }
 
   // Récupérer toutes les clés
@@ -32,4 +37,10 @@ public class ActivationKeyController {
   public boolean useKey(@PathVariable String key) {
     return activationKeyService.useKey(key);
   }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        activationKeyRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
