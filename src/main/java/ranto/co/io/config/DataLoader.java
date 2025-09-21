d5bb15eab9f292e19f7a6ce40a57aabba74e7465
h-ranto <hei.ranto.2@gmail.com>
2025-09-21 19:45:19 +0300

    decomment Dataloader

--- src/main/java/ranto/co/io/config/DataLoader.java
+++ src/main/java/ranto/co/io/config/DataLoader.java
@@ -1,24 +1,20 @@
-// package ranto.co.io.config;
-//
-// import lombok.RequiredArgsConstructor;
-// import org.springframework.boot.CommandLineRunner;
-// import org.springframework.stereotype.Component;
-// import ranto.co.io.model.ActivationKey;
-// import ranto.co.io.service.ActivationKeyService;
-//
-// @Component
-// @RequiredArgsConstructor
-// public class DataLoader implements CommandLineRunner {
-//
-//  private final ActivationKeyService activationKeyService;
-//
-//  @Override
-//  public void run(String... args) {
-//    // Exemple : clé fixe valable 7 jours
-//    activationKeyService.createKey("ABC123XYZ", 7);
-//
-//    // Ou bien générer une clé aléatoire
-//    ActivationKey randomKey = activationKeyService.generateKey();
-//    System.out.println("Generated activation key: " + randomKey.getKeyValue());
-//  }
-// }
+ package ranto.co.io.config;
+
+ import lombok.RequiredArgsConstructor;
+ import org.springframework.boot.CommandLineRunner;
+ import org.springframework.stereotype.Component;
+ import ranto.co.io.model.ActivationKey;
+ import ranto.co.io.service.ActivationKeyService;
+
+ @Component
+ @RequiredArgsConstructor
+ public class DataLoader implements CommandLineRunner {
+
+  private final ActivationKeyService activationKeyService;
+
+  @Override
+  public void run(String... args) {
+    // Exemple : clé fixe valable 7 jours
+    activationKeyService.createKey("ABC123XYZ", 1);
+  }
+ }
