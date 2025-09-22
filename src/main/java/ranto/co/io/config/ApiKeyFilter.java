a862c6908e673ad66311ca698a3b20cbab6d22ef
h-ranto <hei.ranto.2@gmail.com>
2025-09-22 14:24:03 +0300

    feat: use api key for public endpoint

---
+++ src/main/java/ranto/co/io/config/ApiKeyFilter.java
@@ -0,0 +1,46 @@
+package ranto.co.io.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Order(0) // s'exécute avant JwtFilter
+public class ApiKeyFilter extends OncePerRequestFilter {
+
+    @Value("${app.api.key}") // injectée depuis application.properties
+    private String validApiKey;
+
+    private static final String API_KEY_HEADER = "X-API-KEY";
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+        String path = request.getRequestURI();
+
+        // Vérifie uniquement pour /api/auth/? et /pingR
+        if (path.equals("/api/auth/register")
+                || path.equals("/api/auth/login")
+                || path.equals("/pingR")) {
+            String apiKey = request.getHeader(API_KEY_HEADER);
+
+            if (apiKey == null || !apiKey.equals(validApiKey)) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write("Invalid or missing API key");
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
