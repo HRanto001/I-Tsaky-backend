e8fd4f7ed0a45d7df3e44ec553fcb37a6c3f80de
h-ranto <hei.ranto.2@gmail.com>
2025-12-15 22:21:22 +0300

    chore: use lombok constructor

--- src/main/java/ranto/co/io/mail/EmailRequest.java
+++ src/main/java/ranto/co/io/mail/EmailRequest.java
@@ -2,8 +2,10 @@ package ranto.co.io.mail;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@AllArgsConstructor
 @Data
 public class EmailRequest {
     @NotBlank @Email private String to;
