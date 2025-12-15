3c08fdd5a46977dbb86a8b08dbc01cc3188f0260
h-ranto <hei.ranto.2@gmail.com>
2025-12-15 11:35:28 +0300

    chore: add new api url for email sender access for api key

--- src/main/java/ranto/co/io/config/SecurityConfig.java
+++ src/main/java/ranto/co/io/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
                 auth.requestMatchers(
                         "/api/auth/login",
                         "/api/auth/register",
+                        "/api/emails/send",
                         "/pingR",
                         "/api/auth/reset-password")
                     .permitAll()
