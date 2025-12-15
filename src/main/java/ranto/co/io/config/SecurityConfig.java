648da5841e2ee206751fb01a461b3983b7c653ec
h-ranto <hei.ranto.2@gmail.com>
2025-12-15 22:29:03 +0300

    chore(secu): access for two new endpoint (use api key)

--- src/main/java/ranto/co/io/config/SecurityConfig.java
+++ src/main/java/ranto/co/io/config/SecurityConfig.java
@@ -34,7 +34,9 @@ public class SecurityConfig {
                         "/api/auth/register",
                         "/api/emails/send",
                         "/pingR",
-                        "/api/auth/reset-password")
+                        "/api/auth/reset-password",
+                        "/api/auth/request-activation-key",
+                        "/api/auth/request-reset-password")
                     .permitAll()
                     .anyRequest()
                     .authenticated())
