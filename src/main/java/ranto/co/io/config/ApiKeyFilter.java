812335c1064a95c6caa45328ab06722ba3b95999
h-ranto <hei.ranto.2@gmail.com>
2025-12-16 20:26:17 +0300

    chore: format class apiKeyFilter

--- src/main/java/ranto/co/io/config/ApiKeyFilter.java
+++ src/main/java/ranto/co/io/config/ApiKeyFilter.java
@@ -14,42 +14,41 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @Order(0)
 public class ApiKeyFilter extends OncePerRequestFilter {
 
-    @Value("${app.api.key}")
-    private String validApiKey;
+  @Value("${app.api.key}")
+  private String validApiKey;
 
-    private static final String API_KEY_HEADER = "X-API-KEY";
+  private static final String API_KEY_HEADER = "X-API-KEY";
 
-    @Override
-    protected void doFilterInternal(
-            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
+  @Override
+  protected void doFilterInternal(
+      HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
 
-        String path = request.getRequestURI();
+    String path = request.getRequestURI();
 
-        // Vérifie uniquement pour /api/auth/? et /pingR
-        if (path.startsWith("/api/auth/register")
-                || path.equals("/api/auth/login")
-                || path.equals("/api/auth/reset-password")
-                || path.equals("/api/auth/request-activation-key")
-                || path.equals("/api/auth/request-reset-password")
-                || path.equals("/api/emails/send")
-                || path.equals("/api/auth/check-email")
-                || path.equals("/pingR")) {
-            String apiKey = request.getHeader(API_KEY_HEADER);
+    // Vérifie uniquement pour /api/auth/? et /pingR
+    if (path.startsWith("/api/auth/register")
+        || path.equals("/api/auth/login")
+        || path.equals("/api/auth/reset-password")
+        || path.equals("/api/auth/request-activation-key")
+        || path.equals("/api/auth/request-reset-password")
+        || path.equals("/api/emails/send")
+        || path.equals("/api/auth/check-email")
+        || path.equals("/pingR")) {
+      String apiKey = request.getHeader(API_KEY_HEADER);
 
-            if (path.startsWith("/api/auth/")
-                    || path.equals("/pingR")) {
-                filterChain.doFilter(request, response);
-                return;
-            }
-
-            if (apiKey == null || !apiKey.equals(validApiKey)) {
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                response.getWriter().write("Invalid or missing API key");
-                return;
-            }
-        }
-
+      if (path.startsWith("/api/auth/") || path.equals("/pingR")) {
         filterChain.doFilter(request, response);
+        return;
+      }
+
+      if (apiKey == null || !apiKey.equals(validApiKey)) {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write("Invalid or missing API key");
+        return;
+      }
     }
+
+    filterChain.doFilter(request, response);
+  }
 }
